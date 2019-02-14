fbce68ca75ae33d2b325e284319acdeb7e581e5f
Kirill <kirill.oshkalo.1997@mail.ru>
2019-02-14 17:29:21 +0300

    add comments

--- app.rb
+++ app.rb
@@ -9,33 +9,45 @@ def init_db
 	@db.results_as_hash = true
 end
 
+#before вызывается каждый раз при перезагрузке любой страницы
 before do
+	#инициализация базы данных
 	init_db
 end
+
+#configure вызывается каждый раз при конфигурации приложения:
+#когда изменился код программы и перезагрузилась страница
 
 configure do
+	#инициализация базы данных
 	init_db
+
+	#создаёт таблицу если таблица не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
-		(
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-		 	created_date DATE,
-		 	content TEXT
-	 	)'
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT
+		)'
 
-end
+	end
 
-get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"
-end
+	get '/' do
+		erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"
+	end
 
-get '/new' do
-	erb :new
-end
+	#обработчик get-запроса /new
+	#(браузер получает страницу с сервера)
+	get '/new' do
+		erb :new
+	end
 
-post '/new' do
-	@db.execute
+	post '/new' do
+		#получаем переменную из post-запроса
+		#(браузер отправляет данные на сервер)
+		@db.execute
 
-	content = params[:content]
+		content = params[:content]
 
-	erb "You typed #{content}"
-end
+		erb "You typed #{content}"
+	end
