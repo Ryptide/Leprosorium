182616e083616e968f24c1cefd6c5f9ac3047629
Kirill <kirill.oshkalo.1997@mail.ru>
2019-02-15 14:45:46 +0300

    Redirect to main page while adding a post

--- app.rb
+++ app.rb
@@ -58,5 +58,7 @@ configure do
 
 		@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
+#перенаправление на главную страницу
+redirect to '/'
 		erb "You typed #{content}"
 	end
